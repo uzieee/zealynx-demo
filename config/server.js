b70666d475bff6655189e2a02309b60eca845e76
Usman Shafique <usmanshafique4342@gmail.com>
2024-07-22 05:31:56 +0500

    appkey added

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['a8f5f167f44f4964e6c998dee827110c', 'e99a18c428cb38d5f260853678922e03']),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
